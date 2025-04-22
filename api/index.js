55acbe86d9986ae909b63bc7a95e52ffdac19519
Sabrina Paschini <sabrina.paschini@hotmail.com>
2025-04-22 16:08:26 -0300

    add: primeiras rotas

--- api/index.js
+++ api/index.js
@@ -1,19 +1,38 @@
-const express = require ('express') // importando o express 
+const express = require("express"); // importando o express
 const app = express();
-const port = 3000; 
+const port = 3000;
 
-// midleware para aceitar o Json 
+const cors = require("cors"); // importando o cors, que é um pacote, que permite a conexão entre dois projetos diferentes
+app.use(cors());
 
+// midleware para aceitar o Json
 app.use(express.json());
 
-// ROTA GET 
+// ROTA GET
 
-app.get('/', (req, res) => {
-    res.send('API está funcionando');
+app.get("/componentes", (req, res) => {
+  res.json(componentes);
 });
 
-// iniciando o servidor 
+// iniciando o servidor
+
+let componentes = [];
+
+// primeira rota POST para receber os dados do Angular
+
+app.post("/componentes", (req, res) => {
+  const componente = req.body;
+  componentes.push(componente); // salvando temporariamente
+
+  console.log("Componente recebido", componente);
+
+  // uma resposta de sucesso!
+
+  res
+    .status(201)
+    .json({ mensagem: "Componente recebido com sucesso", componente });
+});
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+  console.log(`Servidor rodando na porta ${port}`);
 });
